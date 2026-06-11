b10310e695f4a29a56ba8184c62922d3bfb7f7e8
Richard <elyson.edu@gmail.com>
2026-06-11 10:49:28 -0300

    Fix python port binding for Render deployment

--- main.py
+++ main.py
@@ -78,4 +78,5 @@ def secret_leads():
     return leads_html
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=False)
